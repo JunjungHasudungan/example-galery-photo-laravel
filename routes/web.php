dcb17fad81ac5d6da7e06dea1cead15c70db4e85
junjunghasudungan <junjunghasudungan43@gmail.com>
2024-07-10 12:34:04 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\Admin\{
+    DashboardController as AdminDashboardController,
+};
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\User\{
+    Dashboardcontroller as UserDashboardController,
+};
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+
+    // ROUTE FOR ADMIN
+    Route::get('admin-dashboard', [AdminDashboardController::class, 'index'])->name('admin-dashboard');
+
+    // ROUTE FOR USER
+    Route::get('user-dashboard', [UserDashboardController::class, 'index'])->name('user-dashboard');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+});
+
+require __DIR__.'/auth.php';
